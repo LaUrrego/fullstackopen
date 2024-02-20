acfa920e8ab9f6d18794a5135c4543b58f8adc86
LaUrrego <lurrego22@gmail.com>
2024-02-19 20:20:45 -0800

    Add note app

--- new-part1/part1/src/App.jsx
+++ new-part1/part1/src/App.jsx
@@ -1,19 +1,53 @@
 import { useState } from "react"
 
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState("Enter a new note...")
+  const [importance, setImportance] = useState(false)
 
-const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const addNote = (e)=> {
+    e.preventDefault()
+    console.log('button clicked: ', e)
+    
+    
+    let noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      important: importance
+    }
+    console.log(noteObject)
+
+    setNotes(notes.concat(noteObject))
+    setImportance(false)
+
+  }
+
+  const updateNote = (e)=>{
+    setNewNote(e.target.value)
+  }
 
   return(
     <div>
-      {left}
-      <button onClick={()=>setLeft(left + 1)} >Left</button>
-      <button onClick={()=>setRight(right +1)}>Right</button>
-      {right}
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <li key={note.id}>{note.content}</li>
+          )}
+      </ul>
+      <form onSubmit={addNote} >
+        <input
+        value={newNote}
+        onChange={updateNote}
+        onClick={()=> setNewNote("")}
+        id="content"
+        />
+        <input type="radio" checked={importance} onClick={()=>setImportance(true)}/>Important?
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
 
 
 export default App
+// npm run dev
